1fcef0ed7cec85ef4dc92e4a9c7951e00f3b0a2c
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-02-15 19:02:46 +0530

    Update healthcare_db.sql

--- healthcare_db.sql
+++ healthcare_db.sql
@@ -1,10 +1,4 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
