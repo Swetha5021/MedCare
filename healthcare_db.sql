5bda4a85d4980889759ba691e0e0b0b3b60d9cee
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-03-10 21:12:37 +0530

    Update healthcare_db.sql

--- healthcare_db.sql
+++ healthcare_db.sql
@@ -8,7 +8,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
+  
 --
 -- Database: `healthcare_db`
 --
