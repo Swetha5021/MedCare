1442f48607cc1ae65135f866aa1385c8cd02fd95
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-03-15 11:32:04 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import sys
    
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare.settings')
-   
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
